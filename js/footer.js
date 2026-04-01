a0acdf8e2e7379cde4cf4bd0056e93a1faa846b9
lukasus55 <lukas.kostyk@gmail.com>
2026-04-01 17:49:52 +0200

    Moved contact section to new page

--- js/footer.js
+++ js/footer.js
@@ -4,14 +4,3 @@ let isMobile = mediaQuery.matches;
 mediaQuery.addEventListener('change', (e) => {
     isMobile = e.matches;
 });
-
-const showContactButton = document.querySelector('#show_contact_button');
-const contactContainer = document.querySelector('#contact_container');
-
-if (showContactButton && contactContainer) {
-    showContactButton.addEventListener("click", () => {
-        // .toggle() adds the class if it's missing, and removes it if it's there. 
-        contactContainer.classList.toggle("show");
-        showContactButton.classList.toggle("active");
-    });
-}
