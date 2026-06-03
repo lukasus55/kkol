17c0b45f6eb40b00845f5f93887e8edf6273bbcc
Łukasz Kostyk <30473680+lukasus55@users.noreply.github.com>
2026-06-03 16:08:40 +0200

    Improve dashboard visuals (#42)
    * Improved dashboard navbar visuals
    
    * Improve login page visuals
    
    * Refactor changePassword form
    
    Changed from onClick event to onSubmit. Additionally added required fields.

--- js/login.js
+++ js/login.js
@@ -1,49 +1,65 @@
 import { appendLoaderDiv } from "./utils/helpers.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-    const container = document.querySelector('.login_card');
-    const loginForm = document.querySelector('#login_form');
+const container = document.querySelector('.login_card');
+const loginForm = document.querySelector('#login_form');
 
-    loginForm.addEventListener('submit', async (event) => {
-        
-        const loadingContainer = appendLoaderDiv(container, 'global_transparent');
+loginForm.addEventListener('submit', async (event) => {
+    
+    const loadingContainer = appendLoaderDiv(container, 'global_transparent');
 
-        // Stop the browser from instantly refreshing the page
-        event.preventDefault(); 
+    // Stop the browser from instantly refreshing the page
+    event.preventDefault(); 
 
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-        try {
-            const response = await fetch('/api/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ username, password }) 
-            });
+    try {
+        const response = await fetch('/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password }) 
+        });
 
-            const data = await response.json();
-            if (response.ok) {
-                console.log("Success:", data.message);
-                
-                // Redirect the user to the private area!
-                window.location.href = '/dashboard'; 
-            } else {
-                container.removeChild(loadingContainer);
-                
-                const loginFailedDiv = document.querySelector('.login_failed')
-
-                if (loginFailedDiv.classList.contains('hiddenInstant')) {loginFailedDiv.classList.remove('hiddenInstant')}
-            }
+        const data = await response.json();
+        if (response.ok) {
+            console.log("Success:", data.message);
             
-        } catch (error) {
+            // Redirect the user to the private area!
+            window.location.href = '/dashboard'; 
+        } else {
             container.removeChild(loadingContainer);
+            
+            const loginFailedDiv = document.querySelector('.login_failed')
 
-            console.error("Network error:", error);
-            alert("Something went wrong communicating with the server.");
+            if (loginFailedDiv.classList.contains('hiddenInstant')) {loginFailedDiv.classList.remove('hiddenInstant')}
         }
-
         
-    });
+    } catch (error) {
+        container.removeChild(loadingContainer);
+
+        console.error("Network error:", error);
+        alert("Something went wrong communicating with the server.");
+    }
 });
+
+const noAccountBtn = document.querySelector('#question-acc');
+const forgotBtn = document.querySelector('#question-forgot');
+
+noAccountBtn.addEventListener('click', () => {
+    showAnswer("Nie masz konta?", "Konta posiadają jedynie gracze uczestniczący w turniejach KKOL. Organizator powinien przekazać dane do logowania. Nie ma możliwości samodzielnego założenia konta.")
+});
+forgotBtn.addEventListener('click', () => {
+    showAnswer("Zapomniałeś hasła?", "Skontaktuj się z administratorem. <a href='/contact'>Lista kontaktów.</a>")
+});
+
+function showAnswer(question, answer) {
+    const answerEl = document.querySelector('#answer');
+    const contentEl = document.querySelector('.answer_content');
+    const titleEl = document.querySelector('.answer_title');
+
+    answerEl.classList.remove('hidden');
+    contentEl.innerHTML = answer;
+    titleEl.textContent = question;
+}
