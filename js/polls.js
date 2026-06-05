0c8ff6ad598cb42ee55977cfc753f3a01d3f8037
lukasus55 <lukas.kostyk@gmail.com>
2026-06-05 14:25:22 +0200

    Add empty labels edit menu

--- js/polls.js
+++ js/polls.js
@@ -1,11 +1,15 @@
 import { formatRelativeTimePL } from "./utils/formatDate.js";
-import { getParamsUrl, requireAuth } from "./utils/helpers.js";
+import { adjustModalPosition, debounce, getParamsUrl, requireAuth } from "./utils/helpers.js";
 
 (async () => {
 
     // Placeholder poll
     const poll = {
-        end_date: "2026-06-04T13:19:00.000Z",
+        end_date: "2026-07-05T13:19:00.000Z",
+        labels: [
+            {name: "Planszówka", hex: "#f7ff80"},
+            {name: "Gra wideo", hex: "#84ff80"}
+        ]
     }
 
     const params = new URLSearchParams(window.location.search);
@@ -16,7 +20,12 @@ import { getParamsUrl, requireAuth } from "./utils/helpers.js";
     const userAuthenticated = await requireAuth(pageUrl);
     if (!userAuthenticated) return;
 
+
+
+
+
     // ====== HEADER ======
+
     const relativeEl = document.querySelector('#poll_date_relative');
 
     const relativeDate = formatRelativeTimePL(poll.end_date);
@@ -27,5 +36,51 @@ import { getParamsUrl, requireAuth } from "./utils/helpers.js";
 
     relativeEl.textContent = relativeDate;
     relativeEl.title = formattedDate;
+
+    const labelsEditorEl = document.querySelector('#poll_labels_editor');
+    const labelEditorToggleBtn = document.querySelector('#btn_toggle_labels_menu');
+
+    labelEditorToggleBtn.onclick = () => {
+        labelEditorToggleBtn.classList.toggle('btn_active');
+        labelsEditorEl.classList.toggle('hidden');
+        adjustModalPosition(labelsEditorEl);
+    }
+
+
+
+
+
+    // ====== UI HANDLERS ======
+
+    function closeAllActionMenus() {
+        const menus = document.querySelectorAll('.poll_action_menu');
+        const menuButtons = document.querySelectorAll('.btn_menu_toggler');
+        menus.forEach(menu => {
+            menu.classList.add('hidden'); 
+        });
+        menuButtons.forEach(btn => {
+            btn.classList.remove('btn_active');
+        })
+    }
+
+    // Close on Escape key press
+    document.onkeydown = (event) => {
+        if (event.key === 'Escape') {closeAllActionMenus();}
+    }
+
+    // Close on outside click
+    document.onmousedown = (event) => {
+        const isInsideMenu = event.target.closest('.poll_action_menu');  
+        if (!isInsideMenu) {closeAllActionMenus();}
+    };
+
+    const handleMenuResize = debounce(() => {
+        const openMenus = document.querySelectorAll('.poll_action_menu:not(.hidden)');
+        openMenus.forEach(menu => {
+            adjustModalPosition(menu);
+        });
+    }, 50)
+
+    window.addEventListener('resize', handleMenuResize);
 
 })()
