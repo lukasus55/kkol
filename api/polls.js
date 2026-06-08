f18cef6a7c4f65ce30ca98c3a7fcdc399d60e284
Łukasz Kostyk <30473680+lukasus55@users.noreply.github.com>
2026-06-08 21:36:22 +0200

    Create poll tab in dashboard (#52)
    * Add polls endpoint
    
    * Renamed polls page to poll
    
    * Add polls list in dashboard
    
    * Install uuidv7
    
    * Add poll_create endpoint
    
    * Add create poll frontend

---
+++ api/polls.js
@@ -0,0 +1,29 @@
+import sql from '../db.js';
+
+export default async function handler(request, response) {
+    try {
+        const { id, player, tournament, limit, order } = request.query;
+        const actualLimit = limit ? Math.min(limit, 100) : 100;
+        const orderClause = sql.unsafe(order === 'reversed' ? 'ORDER BY "end_date" ASC' : 'ORDER BY "end_date" DESC');
+
+        let polls;
+
+        if (id) {polls = await sql`SELECT * FROM "polls" WHERE id = ${id} ${orderClause} LIMIT ${actualLimit}`;}
+        else if (tournament) {polls = await sql`SELECT * FROM "polls" WHERE tournament_id = ${tournament} ${orderClause} LIMIT ${actualLimit}`;}
+        else if (player) {polls = await sql`
+                SELECT p.id, p.create_default_options, p.name, p.end_date, p.rights_level, p.start_date, p.tournament_id, r.player_id
+                FROM "polls" p 
+                INNER JOIN "results" r ON p.tournament_id = r.tournament_id
+                WHERE r.player_id = ${player}
+                ${orderClause}
+                LIMIT ${actualLimit}
+            `;}
+        else {polls = await sql`SELECT * FROM "polls" ${orderClause} LIMIT ${actualLimit}`};
+
+        return response.status(200).json(polls);
+
+    } catch (error) {
+        console.error("Failed to load polls:", error);
+        return response.status(500).json({ error: "Failed to load polls" });
+    }
+}
