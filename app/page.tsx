969d25ba85453dcfa41cc8e9fa8b33ef10a4750f
Łukasz Kostyk <lukas.kostyk@gmail.com>
2026-08-22 23:16:54 +0200

    add events section to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import Link from 'next/link';
 import Footer from '../components/Footer';
+import EventsSection from '@/components/home/EventsSection';
 
 export default function Home() {
   return (
     <>
-      <main>
+      <main className='flex w-full flex-col justify-center items-center pt-16 gap-24'>
+
+        <img
+          src="/img/logos/kol-logo-horizontal.svg"
+          alt="Karwińska Olimpiada Logo"
+          className="h-12 w-auto md:h-16"
+        />
+
+        <EventsSection />
 
       </main>
     </>
